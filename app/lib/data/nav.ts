e06459244ccc464096399900d03bed1c7fbd7a6e
Daniel Fraga <dan.fraga33@gmail.com>
2024-12-21 17:40:16 +1100

    feat: add dummy datta

---
+++ app/lib/data/nav.ts
@@ -0,0 +1,66 @@
+export const NavData = {
+  navMain: [
+    {
+      title: "The Flow",
+      url: "/Dashboard",
+      items: [
+        {
+          title: "Getting Started",
+          url: "/Dashboard",
+        },
+        { title: "Education", url: "/Education" },
+      ],
+    },
+    {
+      title: "Income",
+      url: "/IncomeOverview",
+      items: [
+        {
+          title: "SaaS",
+          url: "/SaaS",
+        },
+        {
+          title: "Job",
+          url: "/Job",
+        },
+        {
+          title: "E-commerce",
+          url: "/Ecommerce",
+        },
+      ],
+    },
+    {
+      title: "Expenses",
+      url: "/ExpenseOverview",
+      items: [
+        {
+          title: "Tax Strategies",
+          url: "/TaxStrategies",
+        },
+      ],
+    },
+    {
+      title: "Invest",
+      url: "/InvestOverview",
+      items: [
+        {
+          title: "Public Equities",
+          url: "/PublicEquities",
+        },
+        {
+          title: "Real Estate",
+          url: "/RealEstate",
+        },
+        {
+          title: "Venture Capital",
+          url: "/VentureCapital",
+        },
+
+        {
+          title: "Savings Account",
+          url: "/SavingsAccount",
+        },
+      ],
+    },
+  ],
+};
