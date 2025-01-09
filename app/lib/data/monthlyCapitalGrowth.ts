8b84c847318b1f3455988b16a9c5dda1dd37a049
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-09 12:58:58 +1100

    chore: add constant data

---
+++ app/lib/data/monthlyCapitalGrowth.ts
@@ -0,0 +1,8 @@
+export const capitalGrowthData = [
+  { month: "January", growth: 2000 },
+  { month: "February", growth: 2200 },
+  { month: "March", growth: 2500 },
+  { month: "April", growth: 2800 },
+  { month: "May", growth: 3100 },
+  { month: "June", growth: 3500 },
+];
---
+++ app/lib/data/monthlyIncomeData.ts
@@ -0,0 +1,8 @@
+export const monthlyIncomeData = [
+  { month: "Jan", income: 25000 },
+  { month: "Feb", income: 27000 },
+  { month: "Mar", income: 28500 },
+  { month: "Apr", income: 26000 },
+  { month: "May", income: 29000 },
+  { month: "Jun", income: 31000 },
+];
