88cd87b777e2737b74b96894c6faca750bc0958d
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-09 18:31:36 +1100

    chroe: add style to param for fn

--- app/utils/pageUtils.ts
+++ app/utils/pageUtils.ts
@@ -1,4 +1,4 @@
-export function getPageCategory(requestUrl) {
+export function getPageCategory(requestUrl: string): string {
   const url = new URL(requestUrl);
   let pageCategory = url.pathname.replace(/^\//, "");
   pageCategory = pageCategory.replace(/^\w/, (c) => c.toUpperCase());
