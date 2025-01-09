a4f483a9d621c702a9d149407e625231e26362ef
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-09 18:29:40 +1100

    refactor: add adccategory btn

---
+++ app/components/AddCategory.tsx
@@ -0,0 +1,43 @@
+import { Plus } from "lucide-react";
+
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { NavData } from "~/lib/data/nav";
+import { addItemToIncome } from "~/utils/addNewItem";
+
+const AddCategory = () => {
+  const navData = NavData.navMain;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button>
+          <Plus />
+          Add
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-56">
+        {navData.map((item) =>
+          item.items.map((subitem) => (
+            <DropdownMenuGroup key={subitem.title}>
+              <DropdownMenuCheckboxItem
+                onClick={() => addItemToIncome(subitem)}
+                checked={subitem.isActive}
+              >
+                {subitem.title}
+              </DropdownMenuCheckboxItem>
+            </DropdownMenuGroup>
+          )),
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default AddCategory;
