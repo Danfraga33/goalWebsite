51d02247664fc93b7868500bda4e3e83fed698a6
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-15 12:17:30 +1100

    chore: add getParent Functions

--- app/utils/pageUtils.ts
+++ app/utils/pageUtils.ts
@@ -4,3 +4,16 @@ export function getPageCategory(requestUrl: string): string {
   pageCategory = pageCategory.replace(/^\w/, (c) => c.toUpperCase());
   return pageCategory;
 }
+
+export function getParentPath(urlPath: string) {
+  // Split the URL path into segments
+  const pathSegments = urlPath.split("/").filter(Boolean); // filter(Boolean) removes empty segments
+
+  // Return the first segment as the parent
+  return pathSegments[0] || "";
+}
+
+export function getSubCategory(category: string) {
+  const parts = category.split("/"); // Split the string by "/"
+  return parts.length > 1 ? parts[1] : category; // Return the second part (Fundamentals), or the full category if no "/"
+}
