883e96497e03bf4f99a57ffc0b595450d80386cc
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-15 16:29:30 +1100

    fix: WIP Cashboard

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,47 +1,15 @@
+import WorkflowChart from "~/components/Workflow";
 import PageTitle from "~/components/PageTitle";
 import Sidebar from "~/components/sidebar";
 import Evernote from "~/components/Evernote";
-import { useLoaderData } from "@remix-run/react";
-import { db } from "~/lib/db/db";
-import { json, ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
-import { NoteCategory } from "@prisma/client";
-import { getPageCategory } from "~/utils/pageUtils";
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  const pageCategory = getPageCategory(request.url);
-  const notes = await db.note.findMany({
-    where: {
-      category: pageCategory as NoteCategory,
-    },
-  });
-
-  if (!notes) throw new Response("Not Found", { status: 404 });
-  return json({ notes });
-}
-
-export async function action({ request }: ActionFunctionArgs) {
-  const pageCategory = getPageCategory(request.url);
-  const formData = await request.formData();
-  const addNote = await db.note.create({
-    data: {
-      userId: 1,
-      category: pageCategory as NoteCategory,
-      title: formData.get("title") as string,
-      content: formData.get("content") as string,
-    },
-  });
-
-  return json({ success: true, addNote });
-}
 
 export default function Dashboard() {
-  const { notes } = useLoaderData<typeof loader>();
   return (
     <Sidebar>
       <div className="flex flex-col gap-4 p-4">
         <PageTitle>Getting Started</PageTitle>
         {/* <WorkflowChart /> */}
-        <Evernote notesData={notes} />
+        <Evernote />
       </div>
     </Sidebar>
   );
