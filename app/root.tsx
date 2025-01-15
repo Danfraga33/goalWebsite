41040d0e2b6686a841daed3e81bb4e421483de87
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-15 17:13:29 +1100

    chore: add as to stylesheet import

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import "./tailwind.css";
 import { ThemeProvider } from "./components/theme-provider";
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: "/app/tailwind.css" },
+  { rel: "stylesheet", href: "/app/tailwind.css", as: "style" },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
@@ -26,6 +26,7 @@ export const links: LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    as: "style",
   },
 ];
 
