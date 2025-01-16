e5fa9ef3e888711175c67328207d0d8235c62348
Daniel Fraga <dan.fraga33@gmail.com>
2025-01-16 12:00:22 +1100

    feat: add category Card on centre/fundamentals

---
+++ app/components/AddCategoryCard.tsx
@@ -0,0 +1,52 @@
+import { Form } from "@remix-run/react";
+import { Plus } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Textarea } from "./ui/textarea";
+
+export function AddCategory() {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <div className="cursor-pointer rounded-lg border bg-black text-white shadow-md">
+          <div className="flex flex-col items-center p-6">
+            <h3 className="mb-2 text-lg font-semibold">Add Category</h3>
+            <Plus />
+          </div>
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create new Category</DialogTitle>
+          <DialogDescription>
+            Create a new category only when a new facet of the study is learned
+            to keep notes organized.
+          </DialogDescription>
+        </DialogHeader>
+        <Form method="POST" className="flex flex-col gap-2">
+          <Label htmlFor="title">Title</Label>
+          <Input id="title" name="titlee" />
+          <Label htmlFor="description">Description</Label>
+          <Textarea id="description" name="description" />
+          <Button>Save</Button>
+          <input
+            type="text"
+            value="addCategory"
+            hidden
+            readOnly
+            name="intent"
+          />
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
